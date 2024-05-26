0317e43df4762c19e278ef7d272e4878d9c1e11e
arvind <achurox1121@gmail.com>
2024-05-26 15:59:55 +0530

    create dynamic table

---
+++ main.js
@@ -0,0 +1,37 @@
+const Books = [];
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function(){
+        return `${title} is written by ${author}, has ${pages} pages and ${read} it.`
+    }
+}
+function addBook(book){
+    Books.push(book);
+}
+addBook(new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'read'))
+addBook(new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 398, 'read'))
+addBook(new Book('The Two Towers', 'J.R.R. Tolkien', 327, 'read'))
+
+function updateTable(){
+    let books = document.getElementsByClassName('heading')[0]
+    let fields = ['title', 'author', 'pages', 'read']
+    for(i=0; i<Books.length; i++){
+        let newBook = document.createElement('div')
+        newBook.className = 'book'
+        fields.forEach(field => {
+            let item = document.createElement('li')
+            item.textContent = Books[i][field]
+            newBook.appendChild(item)
+        })
+        books.appendChild(newBook);
+    }
+    
+}
+updateTable()
+for(i = 0; i < Books.length; i++){
+    console.log(Books[i].info());
+}
